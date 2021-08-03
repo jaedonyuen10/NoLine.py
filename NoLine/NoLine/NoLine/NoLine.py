6a679c6f6256858e2f663e6930ef43669261b221
jaedonyuen10 <33953207+jaedonyuen10@users.noreply.github.com>
2021-08-03 18:28:38 +0800

    NoLine update 0.7
    New beta app

--- NoLine/NoLine/NoLine/NoLine.py
+++ NoLine/NoLine/NoLine/NoLine.py
@@ -6,6 +6,8 @@ import os
 print("os imported")
 import sys 
 print("sys imported")
+import secrets
+print("secrets SSRNGS imported")
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfilename
@@ -13,10 +15,11 @@ print("tkinker imported, renamed to tk")
 datetime.datetime.now()
 datetime.datetime(2009, 1, 6, 15, 8, 24, 78915)
 helpUseage = 0
-NoLineVer = "(bug fix ver 2)]"
+NoLineVer = "0.7]"
 print(" ")
 print("[NoLine ver " + NoLineVer)
 SelectedPin = "0000"
+SecretSafepin = "1234"
 
 Pin = input("Enter Pin >")
 
@@ -46,6 +49,7 @@ if Pin == SelectedPin:
                 print("customCreate - creates a new .txt document for you to put content in (the new faile will be in the same folder as the OS)")
                 print("customRemove - removes a file, USE AT YOUR OWN RISK, NOLINE IS NOT RESPONSIBLE FOR ANY LOSS OF IMPORTANT INFORMATION (can oly remove files inside the folder the os is in)")
                 print("tkNotepad - an aplication version of customCreate (the new file will be in the same folder as the OS)")
+                print("secretSafe - an aplication to test the secret module, can be used to store private stuff. (DO NOT USE NOLINE TO STORE REALLY, REALLY, REALLY SENSITIVE INFORMATION.")
 
                 print("about - about this OS")
                 print("whatsNew - update documentation")
@@ -317,15 +321,75 @@ if Pin == SelectedPin:
              window.mainloop()
              print("closed")
              print(" ")
-        
-
+        elif command == "secretSafe":
+            HasAccess = False
+            RandToken = secrets.token_hex(10)
+            def Program():
+                 print(" ")
+                 print("Welcome to secret safe")
+                 print(" ")
+                 entries = os.listdir(os.getcwd() + "\SecretSafe")
+                 print("[" + os.getcwd() + "]")
+                 for entry in entries:
+                    print(entry)
+                 print(" ")
+                 response = input("State directory name >")
+                 if response == "exit":
+                    print("exited")
+                    print(" ")
+                 else:
+                    if os.path.exists(response):
+                        print(" ")
+                        f = open(response, "r")
+                        print(f.read())
+                        print(" ")
+                        input("Waiting for an input so the OS can close the file...>")
+                        f.close()
+                    else:
+                        print("File Does not exsist")
+                        print(" ")
+            
+            def CheckPassWord():
+                
+                passwordGotten = entry.get()
+                if passwordGotten == SecretSafepin:
+                    print("Correct Pin") #debugging puporses, del  later
+                    text['text'] = "Please enter the random token: " + RandToken
+                    print(RandToken)
+                    button['command'] = CheckRadNumber
+                else:
+                    text['text'] = "Incorrect Password"
+                    
+            def CheckRadNumber():
+               rnGotten = entry.get()
+               
+               if secrets.compare_digest(rnGotten, RandToken) == True:
+                   print("Correct token")
+                   
+                   window.destroy()
+                   Program()
+               else:
+                   window.destroy()
+                   print("You did not enter the correct token")
+                   print(rnGotten+"|")
+                   print(RandToken+"|")
+                   
+                    
+            window = tk.Tk()
+            
+            window.title("SecretSafe Accesss Interface")
+            text = tk.Label(text = "Enter your SecretSafe Password")
+            entry = tk.Entry()
+            button = tk.Button(text = "Ok", command = CheckPassWord)
+            text.pack()
+            entry.pack()
+            button.pack()
+            window.mainloop()
+            
         elif command == "whatsNew":
-            print("as of [2021-8-1][ver " + NoLineVer)
-            print("this command you are using has been added.")
-            print("tkNotepad has been added. <help> for more info!")
-            print("bug fixes. (like crashing when opening a non-exsistant file. [NEW FIX]when naming a file and invalid file name (eg: con), will now just show an error instead of crashing. )")
-            print("tkNotepad Improved")
-            print("now NoLine will inculde bugfix versions.")
+            print("as of [2021-8-3][ver " + NoLineVer)
+            print("""NoLine now has a beta aplication called Secret Safe. This will be in the wiki once everything about Secret safe is done.
+Bug fixes""")
             print(" ")
         elif command == "exit":
             print("exiting...")
---
+++ NoLine/NoLine/NoLine/SecretSafe/README.txt
@@ -0,0 +1,13 @@
+Secret Safe
+============
+the deafult password for Secret safe is 1234.
+============
+this is README for secret safe
+============
+Secret Safe is an application that NoLine uses to secure your documents in text based keys.
+Its 2FA, where one is your password and the other is a string of lines called a token
+============
+WARNING
+DO NOT use Secret safe for real private documents! Secret safe is not as secure as othe methods!
+============
+if you are reading this from the windows file explorer, please do not delete this folder or secretSafe will crash NoLine.
